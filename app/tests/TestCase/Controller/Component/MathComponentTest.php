dc774d21c76aafdfeaa7f8de84a3c22d4fba2754
itosho <www.itosho@gmail.com>
2021-09-12 04:12:47 +0900

    Fix setUp

--- app/tests/TestCase/Controller/Component/MathComponentTest.php
+++ app/tests/TestCase/Controller/Component/MathComponentTest.php
@@ -5,6 +5,10 @@ namespace App\Test\TestCase\Controller\Component;
 
 use App\Controller\Component\MathComponent;
 use Cake\Controller\ComponentRegistry;
+use Cake\Controller\Controller;
+use Cake\Event\Event;
+use Cake\Http\Exception\BadRequestException;
+use Cake\Http\ServerRequest;
 use Cake\TestSuite\TestCase;
 
 /**
@@ -23,7 +27,11 @@ class MathComponentTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $registry = new ComponentRegistry();
+        $request = new ServerRequest([
+            'query' => ['amount1' => 100, 'amount2' => 200]
+        ]);
+        $controller = new Controller($request);
+        $registry = new ComponentRegistry($controller);
         $this->mathComponent = new MathComponent($registry);
     }
 
@@ -51,5 +59,36 @@ class MathComponentTest extends TestCase
         $actual = $this->mathComponent->doComplexOperation($amount1, $amount2);
 
         $this->assertSame($amount1 + $amount2, $actual, 'Total Amount is wrong');
+    }
+
+    /**
+     * beforeFilter() test case
+     *
+     * @doesNotPerformAssertions
+     */
+    public function testBeforeFilter(): void
+    {
+        $event = new Event('Controller.beforeFilter');
+        $this->mathComponent->beforeFilter($event);
+    }
+
+    /**
+     * beforeFilter() test case when bad request
+     *
+     * @return void
+     */
+    public function testBeforeFilterWhenBadRequest(): void
+    {
+        $this->expectException(BadRequestException::class);
+
+        $request = new ServerRequest([
+            'query' => ['amount1' => 100, 'amount3' => 200]
+        ]);
+        $controller = new Controller($request);
+        $registry = new ComponentRegistry($controller);
+        $component = new MathComponent($registry);
+        $event = new Event('Controller.beforeFilter');
+
+        $component->beforeFilter($event);
     }
 }
